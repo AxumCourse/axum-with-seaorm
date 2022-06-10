864ae28ad153bed73f8029227aca7a70b3d2bf69
axumrs <rolson867@gmail.com>
2022-06-10 11:29:38 +0800

    插入更多示例数据

--- app.sql
+++ app.sql
@@ -17,3 +17,26 @@ CREATE TABLE articles ( -- 文章
 INSERT INTO categoies (id,name) VALUES
 (1,'Rust'), (2,'Go'), (3,'Javascript');
 
+-- 插入更多示例数据
+ALTER SEQUENCE categoies_id_seq RESTART WITH 4;
+INSERT INTO categoies (name) VALUES
+('U3A0CsWdiy'),
+('SWACTQFa0Y'),
+('GYqfhaKJ6J'),
+('0sjsXVArdZ'),
+('MiN8lR1g9B'),
+('oBorPeyIvH'),
+('cqS4jGnmxG'),
+('dc0qqvbDNP'),
+('jq8K6LgUFy'),
+('K1tKtlvzgf'),
+('Z5kEYZYEdp'),
+('y3K6ryqRMF'),
+('hwPu60bq1u'),
+('2Idzt9CmAV'),
+('vbLGfMJNHz'),
+('6tTPkRtpWB'),
+('sWBfrpOAIB'),
+('zgmXGcYsGt'),
+('WH2EBpojIS'),
+('m1rsNTknqS');
